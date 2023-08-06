6fa0f43f4265983ebef8d1e30ca3d3af5eff2bfc
Mahmoud Abdelsamad <m.m.abdelsamad82@gmail.com>
2023-08-06 21:03:51 +0200

    Create bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/custom_widgets/add_bottom_sheet.dart';
 import '../custom_widgets/custom_app_bar.dart';
 import '../custom_widgets/notes_list_view.dart';
 
@@ -8,24 +9,37 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {  },
-        child: Icon(Icons.add,color: Colors.white,size: 32,),),
-      body:Padding(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(24.0),
+                      topLeft: Radius.circular(24.0))),
+              context: context,
+              builder: (builder) {
+                return AddBottomSheet();
+              });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 32,
+        ),
+      ),
+      body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            CustomAppBar(),
-            Expanded(child: Padding(
+        child: Column(children: [
+          CustomAppBar(),
+          Expanded(
+            child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: NotesListView(),
             ),
-            )]
-        ),
+          )
+        ]),
       ),
     );
-
   }
 }
-
-
