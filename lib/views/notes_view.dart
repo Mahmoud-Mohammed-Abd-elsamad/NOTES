178edcecc92e1e84d20da4ae24c05d03c699532a
Mahmoud Abdelsamad <m.m.abdelsamad82@gmail.com>
2023-08-06 12:18:10 +0200

    Custom Notes Item

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../custom_widgets/custom_app_bar.dart';
+import '../custom_widgets/custom_notes_item.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class NotesView extends StatelessWidget {
         child: Column(
           children: [
             CustomAppBar(),
+            NotesItem(),
           ]
         ),
       ),
