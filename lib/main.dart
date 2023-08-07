c978c8c9d3b1e90ad338de4f537cf660a6e39aef
Mahmoud Abdelsamad <m.m.abdelsamad82@gmail.com>
2023-08-07 12:44:01 +0200

    add Hive Package and TypeAdapterGenerator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/models/notes_view_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  // Open a Hive Box
+  await Hive.openBox<NotesViewModel>('note');
+
   runApp(const NotesApp());
 }
 
--- lib/models/notes_view_model.dart
+++ lib/models/notes_view_model.dart
@@ -1,11 +1,21 @@
 
-class NotesViewModel {
+import 'package:hive_flutter/adapters.dart';
+
+part 'notes_view_model.g.dart';
+@HiveType(typeId: 0)
+class NotesViewModel extends HiveObject{
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
-  NotesViewModel({ required this.color, required this.title,required  this.content,required this.date});
+  NotesViewModel({ required this.color,
+    required this.title,required  this.content,
+    required this.date});
 
 
 
