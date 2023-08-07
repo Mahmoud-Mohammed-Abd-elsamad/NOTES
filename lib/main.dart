ee3407e71a78faa95afd672a663a3a45c344e940
Mahmoud Abdelsamad <m.m.abdelsamad82@gmail.com>
2023-08-08 00:14:09 +0200

    register type adapter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/cubits/add_note/add_note_cubit.dart';
+import 'package:notes_app/models/notes_view_model.dart';
 import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
@@ -11,8 +12,9 @@ import 'constants.dart';
 void main() async {
   await Hive.initFlutter();
 
+  Hive.registerAdapter(NotesViewModelAdapter());
   // Open a Hive Box
-  await Hive.openBox(kNotesBoc);
+  await Hive.openBox<NotesViewModel>(kNotesBoc);
 
   Bloc.observer = SimpleBlocObserver();
   runApp(const NotesApp());
