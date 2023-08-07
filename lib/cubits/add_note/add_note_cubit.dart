9e040c8d77da84ac70ae61fa766d1af0e95f1772
Mahmoud Abdelsamad <m.m.abdelsamad82@gmail.com>
2023-08-07 23:55:07 +0200

    cratae addNote Method

--- lib/cubits/add_note/add_note_cubit.dart
+++ lib/cubits/add_note/add_note_cubit.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note/add_note_state.dart';
 
+import '../../models/notes_view_model.dart';
+
 class AddNoteCubit extends Cubit<AddNoteState>{
-  AddNoteCubit(super.initialState);
+  AddNoteCubit():super(AddNoteStateInitial());
+
+addNote(NotesViewModel note){
 
 
-
+}
 }
