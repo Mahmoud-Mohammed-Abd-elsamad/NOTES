11003d7d483dfb3c0386209a5bb476bfbfd10d47
Mahmoud Abdelsamad <m.m.abdelsamad82@gmail.com>
2023-08-07 11:56:10 +0200

    Create Notes View Model

---
+++ lib/models/notes_view_model.dart
@@ -0,0 +1,10 @@
+class NotesViewModel{
+  final String title;
+  final String content;
+  final int date;
+
+  NotesViewModel({required this.title,required  this.content,required this.date});
+
+
+
+}
